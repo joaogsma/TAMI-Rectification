b069bd1660383c66ddf18e3e8f9da681d1ac8c6c
Luiz Gustavo da Rocha Charamba <lgrc@cin.ufpe.br>
2016-07-18 00:29:48 -0300

    Fazendo a retificação métrica

--- retificacao/line_builder.py
+++ retificacao/line_builder.py
@@ -1,24 +1,27 @@
 import matplotlib.pyplot as plt
 from point import Point
 
+
+
 class Line_Builder:
-    def __init__(self, fig, ax, n_lines, col_num, row_num):
+    def __init__(self, fig, ax, n_lines, col_num, row_num, crossRatio=False):
         self.ax = ax
         self.col_num = col_num
         self.row_num = row_num
         self.n_lines = n_lines
+        self.crossRatio = crossRatio
 
         self.lines = list()
         while len(self.lines) < n_lines:
-            draw_color = ("r" if len(self.lines) < 2 else 
-                          ("g" if len(self.lines) < 4 else 
+            draw_color = ("r" if len(self.lines) < 2 else
+                          ("g" if len(self.lines) < 4 else
                             ("b" if len(self.lines) < 6 else "c")))
             l, = ax.plot([], [], color=draw_color)  # empty line
             self.lines.append(l)
 
         self.cid = fig.canvas.mpl_connect('button_press_event', self)
-        self.count = 0
-
+        self.countLines = 0
+        self.countPoints = 0
         self.x_list = list()
         self.y_list = list()
 
@@ -37,9 +40,8 @@ class Line_Builder:
 
         return points
 
-
     def get_lines(self):
-        if (len(self.x_list) != len(self.y_list) or 
+        if (len(self.x_list) != len(self.y_list) or
                 len(self.x_list) != 2*len(self.lines)):
             raise Exception("Error during line building")
 
@@ -56,28 +58,68 @@ class Line_Builder:
 
         return lines_
 
+    def calcColinearPoint(self, P0, PF, x):
+        P0.normalize()
+        PF.normalize()
+        x0 = P0.x
+        y0 = P0.y
+        xf = PF.x
+        yf = PF.y
+        a = (yf - y0)/(xf - x0)
+        b = y0 - a*x0
+        y = a*x + b
+        P = Point(x,y,1)
+        return P
 
 
     def __call__(self, event):
         if event.inaxes!=self.lines[0].axes:
             return
 
-        if self.count >= 2*self.n_lines:
+        if self.countLines >= 2*self.n_lines:
             return
-        
-        draw_color = ("r" if self.count < 4 else 
-                      ("g" if self.count < 8 else 
-                        ("b" if self.count < 12 else "c")))
 
-        self.x_list.append(event.xdata)
-        self.y_list.append(event.ydata)
-        
-        line = self.lines[int(self.count/2)]
+        draw_color = ("r" if self.countLines < 4 else
+                      ("g" if self.countLines < 8 else
+                        ("b" if self.countLines < 12 else "c")))
 
-        xs = self.x_list[len(self.x_list) - 2 + (len(self.x_list) % 2):]
-        ys = self.y_list[len(self.x_list) - 2 + (len(self.x_list) % 2):]
+        x_ = event.xdata
+        y_ = event.ydata
+
+        if (self.crossRatio and ((self.countPoints == 1) or (self.countPoints == 5))):
+
+            self.x_list.append(x_)
+            self.x_list.append(x_)
+            self.y_list.append(y_)
+            self.y_list.append(y_)
+            self.countPoints += 2
+            self.countLines += 1
+            print("countPoints = ", countPoints)
+        elif (self.crossRatio and ((self.countPoints == 2) or (self.countPoints == 6))):
+            print("AQUI")
+            i = len(self.x_list) - 1
+            PF = Point(self.x_list[i], self.y_list[i], 1)
+            P0 = Point(self.x_list[i-1], self.y_list[i-1], 1)
+            P = self.calcColinearPoint(P0, PF, x_)
+
+            self.x_list.append(P.x)
+            self.y_list.append(P.y)
+            self.countPoints += 1
+        else:
+            self.x_list.append(event.xdata)
+            self.y_list.append(event.ydata)
+            self.countPoints += 1
+
+        line = self.lines[int(self.countLines/2)]
+
+        if (self.crossRatio and (self.countLines == 3) or (self.countLines == 7)):
+            xs = self.x_list[len(self.x_list) - 4 + (len(self.x_list) % 4):]
+            ys = self.y_list[len(self.x_list) - 4 + (len(self.x_list) % 4):]
+        else:
+            xs = self.x_list[len(self.x_list) - 2 + (len(self.x_list) % 2):]
+            ys = self.y_list[len(self.x_list) - 2 + (len(self.x_list) % 2):]
 
         line.set_data(xs, ys)
         self.ax.scatter(xs, ys, c=draw_color)
         line.figure.canvas.draw()
-        self.count += 1
+        self.countLines += 1
